9aba093a879814ba326463d2bfa75906b3801ee8
Satish.Marathe <marathesinpune@gmail.com>
2019-10-19 09:15:35 +1100

    Added enzyme packages for unit testing and unit tests for MusicDetail component

---
+++ src/MusicDetail.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {shallow} from "enzyme";
+import MusicDetail from "./MusicDetail";
+import adapter from "./setupTests";
+
+
+describe("Testing MusicDetail Component", () => {
+    it ("should render component properly",() => {
+        /** in this test we are doing a shallow render of MusicDetail Component 
+         *  we are in control of the test data that gets fed to the component so we know what is to be 
+         *  rendered in expected output.
+         *  So we do a random spot check if a specific <DIV> element with a specific class : div-table-row is rendered or not.
+         */
+        const wrapper = shallow(
+            <MusicDetail test={{bandsArrOp:[
+                {
+                    bandName: "ACDC",
+                    myFestivals: [
+                        {festivalName: "Hard Rock Fest"}
+                    ]
+                }
+            ]}} />
+        );
+        //throw wrapper.debug();
+        expect (wrapper.find("div.div-table-row").exists()).toEqual(true);
+    });
+
+    it ("should render component with specific data properly",() => {
+        /** in this test we are doing a shallow render of MusicDetail Component 
+         *  we are in control of the test data that gets fed to the component so we know what is to be 
+         *  rendered in expected output.
+         *  We check if we find a specific value of a <DIV>.
+         */
+        const wrapper = shallow(
+            <MusicDetail test={{bandsArrOp:[
+                {
+                    bandName: "ACDC",
+                    myFestivals: [
+                        {festivalName: "Hard Rock Fest"}
+                    ]
+                }
+            ]}} />
+        );
+        //throw wrapper.debug();
+        expect (wrapper.find("div.div-table-row").at(1).find("div.div-table-col").at(1).text()).toEqual("ACDC");
+    });
+});
